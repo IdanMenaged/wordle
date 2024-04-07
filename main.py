21ccce80f5abcac3529fb5e8410540871898ff2b
idan menaged <IdanMenaged>
2024-04-07 18:01:09 +0300

    choose word

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-def
+from english_words import get_english_words_set
+
+
+def choose_word():
+    words = get_english_words_set(['gcide', 'web2'], alpha=True, lower=True)
+    word = words.pop()
+    words.add(word)
+    return word
---
+++ test.py
@@ -0,0 +1,11 @@
+import unittest
+from english_words import get_english_words_set
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        ...
+
+
+if __name__ == '__main__':
+    unittest.main()
